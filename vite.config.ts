0969b0df9a87319b346c365be6c2141c86e76a81
CoveMB <bmarquiscom@gmail.com>
2025-02-28 14:24:39 -0500

    Allow connecting deploy plugin with local Defender

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import 'dotenv/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const developmentAllowedEnvironmentVariables = [
+  'DEFENDER_POOL_ID',
+  'DEFENDER_POOL_CLIENT_ID',
+  'DEFENDER_API_URL',
+];
+
+export default defineConfig(({ mode }) => ({
+	plugins: [sveltekit()],
+	define: mode === "development" ? developmentAllowedEnvironmentVariables.reduce((definedEnvironmentVariables, allowedEnvironmentVariable) => ({
+		...definedEnvironmentVariables, 
+		[`process.env.${allowedEnvironmentVariable}`]:JSON.stringify( process.env[allowedEnvironmentVariable]) 
+	}), {}) : undefined,
+}));

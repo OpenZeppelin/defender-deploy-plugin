bb008c53517199a3b2f54662689e929d658b7e10
Cove Marquis-Bortoli <44323490+CoveMB@users.noreply.github.com>
2025-05-26 14:33:05 -0400

    Add backup chain id getter, display clear error message on failing to switch wallet, remove remix log (#58)
    * Add backup chain id getter, display clear error message on failing to switch wallet, remove remix log
    
    * extract logic in function, re-indent

--- src/lib/ethereum/index.ts
+++ src/lib/ethereum/index.ts
@@ -1,44 +1,74 @@
-import { type Eip1193Provider, BrowserProvider, ContractFactory } from 'ethers';
-import { type NetworkResponse, type TenantNetworkResponse } from "$lib/models/network";
-import type { DeployContractResult } from '$lib/models/ethereum';
-import { log } from '$lib/remix/logger';
+import { type Eip1193Provider, BrowserProvider, ContractFactory } from "ethers";
+import {
+  type NetworkResponse,
+  type TenantNetworkResponse,
+} from "$lib/models/network";
+import type { DeployContractResult } from "$lib/models/ethereum";
 
 function getEthereum(): Eip1193Provider {
-  if (!window.ethereum) throw new Error('Injected provider not found');
+  if (!window.ethereum) throw new Error("Injected provider not found");
   return window.ethereum;
+}
+
+async function getCurrentWalletNetwork() {
+  const ethereum = getEthereum();
+
+  try {
+    return await ethereum.request({ method: "eth_chainId" });
+  } catch (err) {
+    try {
+      const netVersion = await ethereum.request({ method: "net_version" });
+      return `0x${parseInt(netVersion as string, 10).toString(16)}`;
+    } catch {
+      throw new Error(`Error switching network: ${(err as Error).message}`);
+    }
+  }
+}
+
+async function requestWalletSwitchChain(
+  network: NetworkResponse | TenantNetworkResponse
+) {
+  const ethereum = getEthereum();
+
+  try {
+    await ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: `0x${network.chainId.toString(16)}` }],
+    });
+  } catch {
+    throw new Error(
+      `Could not automatically switch to ${network.name}. Ensure the appropriate network ${network.name} is selected on your wallet (multiple wallet extensions can also prevent proper deployment with injected provider)`
+    );
+  }
 }
 
 /**
  * Switches the user's wallet to the target network.
- * 
+ *
  * @param network target network to switch to.
  */
-export async function switchToNetwork(network: NetworkResponse | TenantNetworkResponse) {
+export async function switchToNetwork(
+  network: NetworkResponse | TenantNetworkResponse
+) {
   if (!network.chainId) throw new Error(`Invalid network: ${network}`);
 
-  const ethereum = getEthereum();
+  const currentChainIdHex = await getCurrentWalletNetwork();
+  if (parseInt(currentChainIdHex, 16) === network.chainId) return;
 
-  // ignore if user is already connected to target network.
-  const current = await ethereum.request({ method: 'eth_chainId' });
-  if (parseInt(current, 16) === network.chainId) return;
-
-  log("[Defender Deploy] Switching network...");
-
-  await ethereum.request({
-    method: 'wallet_switchEthereumChain',
-    params: [{ chainId: `0x${network.chainId.toString(16)}` }],
-  });
-};
+  await requestWalletSwitchChain(network);
+}
 
 /**
- * 
+ *
  * @param deployTxData contract bytecode generated from contract compilation.
  * @returns tx hash
  */
-export async function deployContract(deployTxData: string): Promise<DeployContractResult> {
+export async function deployContract(
+  deployTxData: string
+): Promise<DeployContractResult> {
   const ethereum = getEthereum();
-  
-  await ethereum.request({ method: 'eth_requestAccounts' });
+
+  await ethereum.request({ method: "eth_requestAccounts" });
 
   const provider = new BrowserProvider(ethereum);
   const signer = await provider.getSigner();
@@ -46,7 +76,7 @@ export async function deployContract(deployTxData: string): Promise<DeployContra
 
   const contract = await factory.deploy();
   const recipt = await contract.deploymentTransaction()?.wait();
-  if (!recipt) throw new Error('Deployment transaction failed');
+  if (!recipt) throw new Error("Deployment transaction failed");
 
   const contractAddress = await contract.getAddress();
   const senderAddress = await signer.getAddress();
